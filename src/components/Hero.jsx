29aa1face82a4961759d57eaf17e15ac0a52fe32
Yakob-B <yakobatechno@gmail.com>
2025-12-12 22:32:28 +0300

    fixed 3D model and text visibility on mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -7,7 +7,7 @@ const Hero = () => {
   return (
     <section className={`relative w-full h-screen mx-auto`}>
       <div
-        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5`}
+        className={`absolute inset-0 top-[120px]  max-w-7xl mx-auto ${styles.paddingX} flex flex-row items-start gap-5 z-10`}
       >
         <div className='flex flex-col justify-center items-center mt-5'>
           <div className='w-5 h-5 rounded-full bg-accent' />
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-accent'>Yakoba</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-             Computer Science Undergrad | React & Node.js Expert | UX-Focused Product Builder <br className='sm:block hidden' />
+            Computer Science Undergrad | React & Node.js Expert | UX-Focused Product Builder <br className='sm:block hidden' />
             Building modern, secure, and animated user experiences
           </p>
         </div>
